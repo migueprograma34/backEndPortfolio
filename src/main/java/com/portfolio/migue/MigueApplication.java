590591bf85cbb80d28f772e8d8dd5898a5462142
migueprograma34 <mr141434@gmail.com>
2023-03-20 20:50:57 -0300

    cors

--- src/main/java/com/portfolio/migue/MigueApplication.java
+++ src/main/java/com/portfolio/migue/MigueApplication.java
@@ -2,12 +2,25 @@ package com.portfolio.migue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MigueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MigueApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://backend-migue.web.app")
+						.allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
